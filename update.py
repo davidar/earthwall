c6a6cd8892c9de6833057575a8aeb3baf3e56364
David A Roberts <d@vidr.cc>
2018-02-18 19:05:02 +1000

    kde plasma support

--- update.py
+++ update.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python
 import os.path
+import dbus
 import requests
 import urllib
 import subprocess
 
 MIN_WIDTH = 4000
 CWD = os.path.dirname(os.path.abspath(__file__))
+
+def plasma_wallpaper(fname):
+    jscript = """
+    var allDesktops = desktops();
+    for(i = 0; i < allDesktops.length; i++) {
+        d = allDesktops[i];
+        d.wallpaperPlugin = "org.kde.image";
+        d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");
+        d.writeConfig("Image", "file://%s")
+    }"""
+    bus = dbus.SessionBus()
+    plasma = dbus.Interface(bus.get_object('org.kde.plasmashell', '/PlasmaShell'),
+                            dbus_interface='org.kde.PlasmaShell')
+    plasma.evaluateScript(jscript % fname)
 
 d = requests.get('https://www.reddit.com/r/EarthPorn/top/.json', headers={'User-agent':'EarthWall'}).json()
 for c in d['data']['children']:
@@ -18,4 +33,5 @@ for c in d['data']['children']:
             print('fetching ' + url + ' ...')
             urllib.urlretrieve(url, fname)
         subprocess.call(["feh", "--bg-fill", fname])
+        plasma_wallpaper(fname)
         break
