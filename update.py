ac4e42b3744065fa763edc8f40fec5f46a9a5231
David A Roberts <d@vidr.cc>
2018-02-18 18:34:37 +1000

    json format changed

--- update.py
+++ update.py
@@ -11,8 +11,11 @@ d = requests.get('https://www.reddit.com/r/EarthPorn/top/.json', headers={'User-
 for c in d['data']['children']:
     src = c['data']['preview']['images'][0]['source']
     if src['width'] >= MIN_WIDTH:
+        print(c['data']['title'])
         fname = os.path.join(CWD, 'cache', c['data']['id'] + '.jpeg')
         if not os.path.exists(fname):
-	    urllib.urlretrieve(src['url'], fname)
+            url = src['url'].replace('&amp;', '&')
+            print('fetching ' + url + ' ...')
+            urllib.urlretrieve(url, fname)
         subprocess.call(["feh", "--bg-fill", fname])
         break
