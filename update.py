b0acc42aaa5cc5e17b763a6e97388bcb7344ffef
David A Roberts <d@vidr.cc>
2018-03-06 18:56:39 +1000

    cgi

--- update.py
+++ update.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+from __future__ import print_function
+import os
 import os.path
-import dbus
+import sys
 import requests
 import urllib
 import subprocess
@@ -9,6 +11,7 @@ MIN_WIDTH = 4000
 CWD = os.path.dirname(os.path.abspath(__file__))
 
 def plasma_wallpaper(fname):
+    import dbus
     jscript = """
     var allDesktops = desktops();
     for(i = 0; i < allDesktops.length; i++) {
@@ -25,12 +28,16 @@ def plasma_wallpaper(fname):
 d = requests.get('https://www.reddit.com/r/EarthPorn/top/.json', headers={'User-agent':'EarthWall'}).json()
 for c in d['data']['children']:
     src = c['data']['preview']['images'][0]['source']
+    url = src['url'].replace('&amp;', '&')
     if src['width'] >= MIN_WIDTH:
-        print(c['data']['title'])
+        if os.getenv('GATEWAY_INTERFACE') == 'CGI/1.1':
+            print('Location:', url)
+            print()
+            break
+        print(c['data']['title'], file=sys.stderr)
         fname = os.path.join(CWD, 'cache', c['data']['id'] + '.jpeg')
         if not os.path.exists(fname):
-            url = src['url'].replace('&amp;', '&')
-            print('fetching ' + url + ' ...')
+            print('fetching', url, '...', file=sys.stderr)
             urllib.urlretrieve(url, fname)
         subprocess.call(["feh", "--bg-fill", fname])
         plasma_wallpaper(fname)
